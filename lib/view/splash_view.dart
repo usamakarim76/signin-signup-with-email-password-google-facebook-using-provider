213784fb42beafb315014f1be51b925869dccea8
Usama <usamakarim076@gmail.com>
2024-04-19 16:11:38 +0500

    Update

--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -1,3 +1,8 @@
+import 'package:example/resources/colors.dart';
+import 'package:example/resources/components/round_button.dart';
+import 'package:example/utils/routes/route_name.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatefulWidget {
@@ -8,8 +13,36 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        checkLogin();
+      },
+    );
+  }
+
+  void checkLogin() {
+    if (auth.currentUser == null) {
+      Navigator.pushNamedAndRemoveUntil(
+          context, RouteNames.loginScreen, (route) => false);
+    } else {
+      Navigator.pushNamedAndRemoveUntil(
+          context, RouteNames.mainScreen, (route) => false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.kPrimaryColor,
+      body: Center(
+        child: loadingWidget(AppColors.kWhiteColor),
+      ),
+    );
   }
 }
